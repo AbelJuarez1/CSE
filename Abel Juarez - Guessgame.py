2286c5da4fe335acd7923dbc623a1612dc5ba941
AbelJuarez1 <34071948+AbelJuarez1@users.noreply.github.com>
2017-12-01 11:34:47 -0800

    started guessgame
    12/1/17

--- Abel Juarez - Guessgame.py
+++ Abel Juarez - Guessgame.py
@@ -1,1 +1,8 @@
-# Abel Juarez
+import random
+print(random.randint(0,50))
+
+
+print("Try to guess the number I'm thinking of.")
+print("You have 5 guess or it's game over.")
+print("Get it right and it's...still...game over...")
+
