8ce76cc27c7550d32de67baaf09d2981b7597f7d
AbelJuarez1 <34071948+AbelJuarez1@users.noreply.github.com>
2017-12-04 10:48:02 -0800

    added to notes
    12/4/17

--- Abel Juarez - Guessgame.py
+++ Abel Juarez - Guessgame.py
@@ -1,8 +1,16 @@
-import random
-print(random.randint(0,50))
+import random  # Abel Juarez
 
+# step one) generate random number
+# step two) take an input (number) from the user
+# step three) compare input to generated number
+# step four) add "higher" and "lower" statements
+# step five) add 5 guesses
 
 print("Try to guess the number I'm thinking of.")
 print("You have 5 guess or it's game over.")
 print("Get it right and it's...still...game over...")
+
+a = 'random.randint'
+print(random.randint(0 + 1,50))
+number = input()
 
---
+++ Warmups.py
@@ -0,0 +1,10 @@
+#  12.4.17
+def reverse_order(last, first):
+    # print("%s, %s" % (last, first))
+    print("last_name + " " + first_name")  # Concatenation
+
+
+def reverse_order():
+    first_name = input("First name")
+    last_name = input("Last name")
+    print("$s, %s" % (last_name, first_name))
