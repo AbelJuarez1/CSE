f360a01e3b9da023538ef2291fe6ea7345b14ad4
AbelJuarez1 <34071948+AbelJuarez1@users.noreply.github.com>
2017-12-08 11:34:30 -0800

    started lucky 7's
    12/8/17

--- Warmups.py
+++ Warmups.py
@@ -1,6 +1,6 @@
 # #  12.4.17
-
-
+#
+#
 # def reverse_order(last_name, first_name):
 #     # print("%s, %s" % (last, first))
 #     print("last_name + " " + first_name")  # Concatenation
@@ -10,32 +10,41 @@
 #     first_name = input("First name")
 #     last_name = input("Last name")
 #     print("$s, %s" % (last_name, first_name))
-#
-#
-# #  12.5.17
 
 
+# # 12.5.17
+#
+#
 # def add_py(name):
 #     print("%s.py" % name)  # Solution 1
 #     print(name + ".py")  # Solution 2
-#
-#
+
+
 # # 12.6.17
-
-
+#
+#
 # def add(num1, num2, num3):
 #     print(num1 + num2 + num3)
 #
 #
 # add(1, 2, 3)
 
-# 12.7.17
+# # 12.7.17
+#
+#
+# def repeat(string):
+#     print(string)
+#     print(string)
+#     print(string)
+#
+#     for x in range(3):
+#         print(string)
 
-
-def repeat(string):
-    print(string)
-    print(string)
-    print(string)
-
-    for x in range(3):
-        print(string)
+# # 12.8.17
+#
+#
+# def date(month, day, year):
+#     print(month + "/" + day + "/" + year)
+#     print("%s/%s/%s" % (month, day, year))
+#
+# date("12", "8", "17")
