1393eed2852fde977cb20c4a3c4906bdb7d1934d
AbelJuarez1 <34071948+AbelJuarez1@users.noreply.github.com>
2018-01-16 10:47:08 -0800

    1/15/18
    redid code

--- Abel Juarez - Lucky 7's.py
+++ Abel Juarez - Lucky 7's.py
@@ -5,9 +5,21 @@ money = 15
 d1 =(random.randint(1, 6))
 d2 =(random.randint(1, 6))
 total = d1 + d2
+print("rolled: %s" % total)
+print("money: %s" % money)
 
-while total <= str(7):
-    money - 1
+while money >= 0:
+    d1 = random.randint(1,6)
+    d2 = random.randint(1,6)
+    total = d1 + d2
+    print("rolled")
+
+
+#while total <= 7:
+
+
+
+
 #
 #
 # str(money) <= str(0):
