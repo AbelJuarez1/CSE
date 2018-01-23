ba738a723af01210a4c543fbb2d0456aa35a2099
AbelJuarez1 <34071948+AbelJuarez1@users.noreply.github.com>
2018-01-23 10:47:05 -0800

    1/23/18
    added to hangman

--- Abel Juarez - Hangman.py
+++ Abel Juarez - Hangman.py
@@ -5,15 +5,16 @@ import string
 A general guide for Hangman
 1. Make a word bank - 10 items
 2. Pick an item from the list
-3. Hide the word (use *)
+3. Add a guess to the list of letters guessed
 4. Reveal letters already guessed
 5. Create the win condition
 """
-
-# print(string.ascii_lowercase)
+guesses = 10
 word_bank = ["Spongebob Squarepants", "Patrick Star", "Sheldon Plankton", "Eugiene Krabs", "Gary the Snail",
              "Computer wife Karen", "Doodlebob", "Squidward Tentacles", "Sandy Cheeks", "Mermaid Man and Barnacle Boy"]
-# random.shuffle()
-# print(word_bank)
-for item in word_bank:
-    print(item)
+print(random.choice(word_bank))
+
+while guesses > 0:
+
+
+if 
