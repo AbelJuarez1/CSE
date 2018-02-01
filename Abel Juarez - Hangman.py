dc635fd90cb7b990f3a138bb30a910401776f1fb
AbelJuarez1 <34071948+AbelJuarez1@users.noreply.github.com>
2018-02-01 10:45:53 -0800

    2/1/18
    added to hangman

--- Abel Juarez - Hangman.py
+++ Abel Juarez - Hangman.py
@@ -9,7 +9,7 @@ A general guide for Hangman
 4. Reveal letters already guessed
 5. Create the win condition
 """
-guesses = 10
+guesses = 11
 guess = " "
 word_bank = ["SpongebobSquarepants", "PatrickStar", "SheldonPlankton", "EugeneKrabs", "GarytheSnail",
              "ComputerwifeKaren", "Doodlebob", "SquidwardTentacles", "SandyCheeks", "BarnacleBoy"]
@@ -17,19 +17,21 @@ letters_guessed = []
 word = random.choice(word_bank)
 correct_letter = list(word)
 
-
-while guesses > 0 != quit:
+while guesses >= 2 != quit:
     output = []
     for letter in word:
         if letter in letters_guessed:
             output.append(letter)
         else:
             output.append("*")
-    print(output)
-
+    print(" ".join(list(output)))
+    if guess not in word:
+        guesses -= 1
+        print("guesses left: %s" % guesses)
     guess = input("Guess a letter: ")
-    print("guesses left: %s" % guesses)
     letters_guessed.append(guess)
     print(" ".join(letters_guessed))
-    if guess in word:
-        guesses -= 1
+
+# if output == word:
+#     print("You win!")
+#     exit(0)
