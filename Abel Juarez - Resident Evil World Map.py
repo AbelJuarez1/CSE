2eff5360a3bf439f97d035fc5579e0fea9643944
AbelJuarez1 <34071948+AbelJuarez1@users.noreply.github.com>
2018-02-14 10:46:57 -0800

    2/14/18
    added remote to map

--- Abel Juarez - Resident Evil World Map.py
+++ Abel Juarez - Resident Evil World Map.py
@@ -1,10 +1,11 @@
 mansion_map = {
     'MAINHALL': {
         'NAME': "Main Hall",
-        'DESCRIPTION': "",
+        'DESCRIPTION': "The Main Hall seems to be very empty. There is a typewriter in the corner. "
+                       "One door is to the west, east, and northeast.",
         'PATHS': {
             'WEST': 'DININGROOM',
-            'EAST': 'SMALLGALLERY',
+            'EAST': 'SGALLERY',
             'NORTHEAST': 'MIRRORROOM1'
         }
     },
@@ -77,5 +78,80 @@ mansion_map = {
         'PATHS': {
             'NORTH': 'NORTHWESTCORRIDOR'
         }
-    }
+    },
+    'MIRROR': {
+        'NAME': "Mirror Room",
+        'DESCRIPTION': "",
+        'PATHS': {
+            'WEST': 'MAINHALL',
+            'SOUTHEAST': 'COSTUME'
+        }
+    },
+    'SGALLERY': {
+        'NAME': "Small Gallery",
+        'DESCRIPTION': "",
+        'PATHS': {
+            'EAST': 'DOG',
+            'WEST': 'MAINHALL'
+        }
+    },
+    'COSTUME': {
+        'NAME': "Costume Room",
+        'DESCRIPTION': "",
+        'PATHS': {
+            'NORTH': 'MIRROR'
+        }
+    },
+    'DOG': {
+        'NAME': "HALLWAY",
+        'DESCRIPTION': "",
+        'PATHS': {
+            'WEST': 'SGALLERY',
+            'NORTH': 'NEROOM'
+        }
+    },
+    'NEROOM': {
+        'NAME': "Northeast Room",
+        'DESCRIPTION': "",
+        'PATHS': {
+            'NORTH': 'BATH',
+            'EAST': 'BOILER',
+            'WEST': 'CROW',
+            'SOUTH': 'DOG',
+            'SOUTHWEST': 'CEILING'
+        }
+    },
+    'BATH': {
+        'NAME': "Bathroom",
+        'DESCRIPTION': "",
+        'PATHS': {
+            'SOUTH': 'NEROOM'
+        }
+    },
+    'BOILER': {
+        'NAME': "Boiler Room",
+        'DESCRIPTION': "",
+        'PATHS': {
+            'WEST': 'NEROOM'
+        }
+    },
 }
+
+current_node = mansion_map['MAINHALL']
+directions = ['NORTH', 'EAST', 'SOUTH', 'WEST', 'NORTHEAST', 'NORTHWEST', 'SOUTHEAST', 'SOUTHWEST']
+
+while True:
+    print(current_node['NAME'])
+    print(current_node['DESCRIPTION'])
+    command = input('>_')
+    if command == 'quit':
+        quit(0)
+    if command in directions:
+        try:
+            name_of_node = current_node['PATHS'][command]
+            current_node = mansion_map[name_of_node]
+        except KeyError:
+            print("You can't go that way.")
+    else:
+        print('Command not recognized')
+        print()
