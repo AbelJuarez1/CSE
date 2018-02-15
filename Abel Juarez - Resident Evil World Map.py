e15ebd17103946bf56028c1affac7a0e2e2764fb
AbelJuarez1 <34071948+AbelJuarez1@users.noreply.github.com>
2018-02-15 10:46:29 -0800

    2/15/18
    added to map

--- Abel Juarez - Resident Evil World Map.py
+++ Abel Juarez - Resident Evil World Map.py
@@ -1,8 +1,8 @@
 mansion_map = {
     'MAINHALL': {
         'NAME': "Main Hall",
-        'DESCRIPTION': "The Main Hall seems to be very empty. There is a typewriter in the corner. "
-                       "One door is to the west, east, and northeast.",
+        'DESCRIPTION': "The Main Hall seems to be very empty. There is a typewriter in the corner to save your "
+                       "progress. One door is to the west, east, and northeast.",
         'PATHS': {
             'WEST': 'DININGROOM',
             'EAST': 'SGALLERY',
@@ -11,7 +11,8 @@ mansion_map = {
     },
     'DININGROOM': {
         'NAME': "Dining Room",
-        'DESCRIPTION': "",
+        'DESCRIPTION': "A big table is in the middle of the room with a blue crystal in the middle."
+                       " One door leads east, and another leads north",
         'PATHS': {
             'EAST': 'MAINHALL',
             'NORTH': 'TEAROOM'
@@ -19,7 +20,8 @@ mansion_map = {
     },
     'TEAROOM': {
         'NAME': "Tea Room",
-        'DESCRIPTION': "",
+        'DESCRIPTION': "You find one of your friends dead on the floor. A zombie walks toward you."
+                       " There is a door to the south, north, and northeast.",
         'PATHS': {
             'SOUTH': 'DINING ROOM',
             'NORTH': 'VULTUREHEAD',
@@ -28,7 +30,8 @@ mansion_map = {
     },
     'PIANOROOM': {
         'NAME': "Piano Room",
-        'DESCRIPTION': "",
+        'DESCRIPTION': "There is a piano with some a music score on it. An ammo clip lays on top of a bar stool."
+                       " The only exit is to the south.",
         'PATHS': {
             'SOUTH': 'TEAROOM'
         }
@@ -45,7 +48,8 @@ mansion_map = {
     },
     'TIGERROOM': {
         'NAME': "Tiger Room",
-        'DESCRIPTION': "There are paintings of tigers everywhere. To the east seems to be a weak wall.",
+        'DESCRIPTION': "A lone statue of a tiger's head stands on the wall. It has a plaque that reads,'Some tigers "
+                       "have a red and blue eye. A door leads south and east.",
         'PATHS': {
             'SOUTH': 'VULTUREHEAD',
             'EAST': 'PLANTROOM'
@@ -53,14 +57,16 @@ mansion_map = {
     },
     'PLANTROOM': {
         'NAME': "Plant Room",
-        'DESCRIPTION': "There is a monstrous plant that seems to be in your way.Past it are some herbs. ",
+        'DESCRIPTION': "There is a monstrous plant that seems to be in your way. Past it are some herbs. The only exit"
+                       "is to the west.",
         'PATHS': {
             'WEST': 'TIGERROOM'
         }
     },
     'KEEPERSROOM': {
         'NAME': "Keeper's Bedroom",
-        'DESCRIPTION': "",
+        'DESCRIPTION': "The room is small with a big bed right in the middle. A body is laying on the floor, "
+                       "and a diary with a key lay open on the desk. The onlt eit is to the east.",
         'PATHS': {
             'EAST': 'VULTUREHEAD'
         }
@@ -74,7 +80,8 @@ mansion_map = {
     },
     'SAFEROOM1': {
         'NAME': "Safe Room",
-        'DESCRIPTION': "There is a box in here to place your items.",
+        'DESCRIPTION': "There is a box in here to place your items and a typewriter to save your progress. The only"
+                       "exit leads north.",
         'PATHS': {
             'NORTH': 'NORTHWESTCORRIDOR'
         }
@@ -104,7 +111,8 @@ mansion_map = {
     },
     'DOG': {
         'NAME': "HALLWAY",
-        'DESCRIPTION': "",
+        'DESCRIPTION': "Your in a long hallway with a couple of ammo clips laying around. Acouple of dogs are blocking"
+                       "the way north. You can go back west if you need.",
         'PATHS': {
             'WEST': 'SGALLERY',
             'NORTH': 'NEROOM'
@@ -135,6 +143,60 @@ mansion_map = {
             'WEST': 'NEROOM'
         }
     },
+    'CROW': {
+        'NAME': "Gallery",
+        'DESCRIPTION': "",
+        'PATHS': {
+            'NORTH': 'SAFEROOM2',
+            'EAST': 'NEROOM',
+            'WEST': 'OUTSIDE',
+            'NORTHWEST': 'STUDY'
+        }
+    },
+    'SAFEROOM2': {
+        'NAME': "Safe Room",
+        'DESCRIPTION': "",
+        'PATHS': {
+            'SOUTH': 'CROW'
+        }
+    },
+    'STUDY': {
+        'NAME': "Study",
+        'DESCRIPTION': "",
+        'PATHS': {
+            'SOUTH': 'CROW'
+        }
+    },
+    'OUTSIDE': {
+        'NAME': "Outside Corridor",
+        'DESCRIPTION': "",
+        'PATHS': {
+            'EAST': 'CROW',
+            'NORTH': 'SHED'
+        }
+    },
+    'SHED': {
+        'NAME': "Shed",
+        'DESCRIPTION': "",
+        'PATHS': {
+            'SOUTH': 'OUTSIDE'
+        }
+    },
+    'CEILING': {
+        'NAME': "Ceiling Room",
+        'DESCRIPTION': "",
+        'PATHS': {
+            'NORTHEAST': 'NEROOM',
+            'SOUTH': 'SHOTGUN'
+        }
+    },
+    'SHOTGUN': {
+        'NAME': "Shotgun Room",
+        'DESCRIPTION': "",
+        'PATHS': {
+            'NORTH': 'CEILING'
+        }
+    },
 }
 
 current_node = mansion_map['MAINHALL']
@@ -144,6 +206,7 @@ while True:
     print(current_node['NAME'])
     print(current_node['DESCRIPTION'])
     command = input('>_')
+    print()
     if command == 'quit':
         quit(0)
     if command in directions:
@@ -152,6 +215,7 @@ while True:
             current_node = mansion_map[name_of_node]
         except KeyError:
             print("You can't go that way.")
+            print()
     else:
         print('Command not recognized')
         print()
