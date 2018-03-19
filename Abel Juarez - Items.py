48cf3ce0e5f473f3fb3785663b3c73b17bf25c7d
AbelJuarez1 <34071948+AbelJuarez1@users.noreply.github.com>
2018-03-19 10:44:53 -0700

    3/19/18
    added to items file

--- Abel Juarez - Items.py
+++ Abel Juarez - Items.py
@@ -1,16 +1,39 @@
 class Item(object):
-    def __init__(self, name, weapon, consumable):
+    def __init__(self, name):
         self.name = name
-        self.weapon = weapon
-        self.consumable = consumable
 
     def use_item(self):
-        print("You used %s" % self.consumable)
+        print("You used the %s" % self.name)
+
+    def drop(self):
+        print("You dropped the %s" % self.name)
+
+
+class Consumable(Item):
+    def __init__(self, name, effect):
+        super(Consumable, self).__init__(name)
+        self.effect = effect
 
 
 class Weapon(Item):
-    def __init__(self, name, weapon, attack):
-        super(Weapon, self).__init__(name, weapon, attack)
+    def __init__(self, name, attack):
+        super(Weapon, self).__init__(name, attack)
         self.attack = attack
 
     def attack(self):
+        print("You attacked the enemy with %s" % self.name)
+
+
+class Knife(Weapon):
+    def __init__(self):
+        super(Knife, self).__init__("Knife", 10)
+
+
+class Gun(Weapon):
+    def __init__(self, name, attack):
+        super(Gun, self).__init__(name, attack)
+
+
+class Handgun(Gun):
+    def __init__(self):
+        super(Handgun, self).__init__("Pistol", 30)
